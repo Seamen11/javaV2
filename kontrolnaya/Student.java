402b91f411ad7da85ab5a5900255dcf9f27d750a
Seamen11 <mnu704@mail.ru>
2022-10-31 14:12:44 +0300

    SQL6

---
+++ kontrolnaya/Student.java
@@ -0,0 +1,26 @@
+package com.kontrolnaya;
+
+import java.util.Scanner;
+
+public class Student
+{
+    private static String name;
+
+    public static String setName()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите имя:");
+        name = in.next();
+        return name;
+    }public static void getName()
+    {
+        System.out.println(name);
+    }
+
+    public static String setAge()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите возраст:");
+        return in.next();
+    }
+}
---
+++ kontrolnaya/worker.java
@@ -0,0 +1,29 @@
+package com.kontrolnaya;
+
+import java.util.Scanner;
+
+public class worker extends Student
+{
+    public static String salary;
+
+    public static String setSalary()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите зарплату:");
+        salary = in.next();
+        return salary;
+    }
+
+    public static void format(String s, String s2, String s3)
+    {
+        System.out.printf("%20s | %-20s | %-20s \n", "ИМЯ", "ВОЗРАСТ", "ЗП");
+        System.out.printf("%20s | %-20s | %-20s \n", s, s2, s3);
+    }
+
+
+    public static void main(String[] args)
+    {
+        format(setName(), setAge(), setSalary());
+    }
+}
+
